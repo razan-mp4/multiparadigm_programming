90063a8ed1c4d0431d8029a55e53a74eb106825b
razan <102546213+razan-mp4@users.noreply.github.com>
2024-05-16 17:43:04 +0100

    Update lab2.rkt

--- lab2/lab2.rkt
+++ lab2/lab2.rkt
@@ -3,13 +3,11 @@
 (require racket/math
          racket/list)
 
-
-
-; Function to sort a list
+; Функція для сортування списку
 (define (sort-list lst)
   (sort lst <))
 
-; Function to get the interval index for a value
+; Функція для отримання індексу інтервалу для значення
 (define (get-interval-index value intervals)
   (let loop ([i 0])
     (cond
@@ -17,46 +15,46 @@
       [(<= value (list-ref intervals i)) i]
       [else (loop (+ i 1))])))
 
-; Function to convert numeric series to linguistic series
+; Функція для конвертації числового ряду в лінгвістичний ряд
 (define (convert-to-linguistic-series numeric-series intervals alphabet-power)
   (for/list ([value numeric-series])
     (integer->char (+ 65 (get-interval-index value intervals)))))
 
-; Function to print linguistic series
+; Функція для виведення лінгвістичного ряду
 (define (print-linguistic-series linguistic-series)
   (printf "Лінгвістичний ряд: ~a\n" (string-join (map (lambda (ch) (string ch)) linguistic-series) " ")))
 
-; Function to create and build matrix for linguistic series
+; Функція для створення та побудови матриці для лінгвістичного ряду
 (define (build-linguistic-matrix linguistic-series)
-  (define size 26) ; Size of the alphabet
-  (define matrix (make-vector size)) ; Create a vector of vectors for the matrix
+  (define size 26) ; Розмір алфавіту
+  (define matrix (make-vector size)) ; Створення вектора векторів для матриці
 
-  ; Initialize the matrix with zeros
+  ; Ініціалізація матриці нулями
   (for ([i (in-range size)])
     (vector-set! matrix i (make-vector size 0)))
 
-  ; Function to convert char to index
+  ; Функція для конвертації символу в індекс
   (define (char->index ch)
     (- (char->integer ch) 65))
 
-  ; Update the matrix with occurrences
+  ; Оновлення матриці з входженнями
   (for ([i (in-range 1 (length linguistic-series))])
     (let* ([prev (char->index (list-ref linguistic-series (- i 1)))]
            [curr (char->index (list-ref linguistic-series i))]
            [current-value (vector-ref (vector-ref matrix prev) curr)])
       (vector-set! (vector-ref matrix prev) curr (+ current-value 1))))
 
-  ; Convert the matrix to a list of lists for printing
+  ; Конвертація матриці у список списків для виведення
   (for/list ([row (in-vector matrix)])
     (vector->list row)))
 
-; Function to print the matrix
+; Функція для виведення матриці
 (define (print-linguistic-matrix matrix)
-  (printf "Linguistic Matrix:\n")
+  (printf "Лінгвістична матриця:\n")
   (for ([row (in-list matrix)])
     (printf "~a\n" (string-join (map number->string row) " "))))
 
-; Function to generate equal interval distribution
+; Функція для генерації рівномірного розподілу інтервалів
 (define (generate-equal-interval-distribution alphabet-power)
   (define interval-size (/ 1.0 alphabet-power))
   (define distribution (make-vector alphabet-power))
@@ -64,7 +62,7 @@
     (vector-set! distribution i interval-size))
   distribution)
 
-; Function to define intervals
+; Функція для визначення інтервалів
 (define (define-intervals distribution)
   (define intervals (make-vector (+ (vector-length distribution) 1)))
   (vector-set! intervals 0 0.0)
@@ -72,18 +70,18 @@
     (vector-set! intervals i (+ (vector-ref intervals (- i 1)) (vector-ref distribution (- i 1)))))
   (vector->list intervals))
 
-; Main function
+; Головна функція
 (define (main)
   (random-seed (current-seconds))
-  (define n 6) ; Number of elements in numeric series
-  (define numeric-series (list 0.0 3.0 1.0 2.0 3.0 1.5)) ; Numeric series
-  (define alphabet-power 2) ; Alphabet power
-  (define distribution (generate-equal-interval-distribution alphabet-power)) ; Probability distribution
-  (define sorted-numeric-series (sort-list numeric-series)) ; Sorted numeric series
-  (define intervals (define-intervals distribution)) ; Intervals
-  (define linguistic-series (convert-to-linguistic-series sorted-numeric-series intervals alphabet-power)) ; Linguistic series
+  (define n 6) ; Кількість елементів у числовому ряді
+  (define numeric-series (list 0.0 3.0 1.0 2.0 3.0 1.5)) ; Числовий ряд
+  (define alphabet-power 2) ; Ступінь алфавіту
+  (define distribution (generate-equal-interval-distribution alphabet-power)) ; Розподіл ймовірності
+  (define sorted-numeric-series (sort-list numeric-series)) ; Відсортований числовий ряд
+  (define intervals (define-intervals distribution)) ; Інтервали
+  (define linguistic-series (convert-to-linguistic-series sorted-numeric-series intervals alphabet-power)) ; Лінгвістичний ряд
   (print-linguistic-series linguistic-series)
-  (define linguistic-matrix (build-linguistic-matrix linguistic-series)) ; Build matrix
-  (print-linguistic-matrix linguistic-matrix)) ; Print matrix
+  (define linguistic-matrix (build-linguistic-matrix linguistic-series)) ; Побудова матриці
+  (print-linguistic-matrix linguistic-matrix)) ; Виведення матриці
 
 (main)
